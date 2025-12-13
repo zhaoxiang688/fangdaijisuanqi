cdaccccc9cf9546b701ea1236aaa46a84e662a63
zhaoxiang688 <152458849+zhaoxiang688@users.noreply.github.com>
2025-12-13 15:29:50 +0800

    Add files via upload

---
+++ tool.js
@@ -0,0 +1,45 @@
+function numericalMul(a, b) {
+    var c = 0,
+        d = a.toString(),
+        e = b.toString();
+    try {
+        c += d.split(".")[1].length;
+    } catch (f) { }
+    try {
+        c += e.split(".")[1].length;
+    } catch (f) { }
+    return Number(d.replace(".", "")) * Number(e.replace(".", "")) / Math.pow(10, c);
+}
+
+// 加法
+function numericalAdd(a, b) {
+    var c, d, e;
+    try {
+        c = a.toString().split(".")[1].length;
+    } catch (f) {
+        c = 0;
+    }
+    try {
+        d = b.toString().split(".")[1].length;
+    } catch (f) {
+        d = 0;
+    }
+    return e = Math.pow(10, Math.max(c, d)), (numericalMul(a, e) + numericalMul(b, e)) / e;
+
+}
+// 减法
+function numericalSub(a, b) {
+    var c, d, e;
+    try {
+        c = a.toString().split(".")[1].length;
+    } catch (f) {
+        c = 0;
+    }
+    try {
+        d = b.toString().split(".")[1].length;
+    } catch (f) {
+        d = 0;
+    }
+    return e = Math.pow(10, Math.max(c, d)), (numericalMul(a, e) - numericalMul(b, e)) / e;
+
+}
